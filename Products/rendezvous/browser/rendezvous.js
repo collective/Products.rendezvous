c506b0b08869c53f7e228986a7186ddccae650b2
vincentfretin <vincentfretin@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-11-17 11:28:00 +0000

    jslint

--- Products/rendezvous/browser/rendezvous.js
+++ Products/rendezvous/browser/rendezvous.js
@@ -1,4 +1,3 @@
-
 var rendezvous = {};
 
 
@@ -6,26 +5,26 @@ rendezvous.init = function(){
 	jq('.template-edit_dates').each(rendezvous.initchoicesselection);
 	jq('.template-rendezvous_view').each(rendezvous.inituserchoice);
 	jq('#rendezvous-choose-date').click(rendezvous.startdatechoice);
-}
+};
 
 rendezvous.initchoicesselection = function(){
 	jq('#rendezvous-dates-select a.calendar-date').click(function(){
 		/* first submit the form in case it has been modified */
 		var formData = jq('form#rendezvous-edit').serialize();
 		var formAction = jq('form#rendezvous-edit').attr('action');
-		jq.post(formAction, formData, function(){})
+		jq.post(formAction, formData, function(){});
 	});
-}
+};
 
 rendezvous.inituserchoice = function(){
-	jq('td.selectable').click(rendezvous.toggleuserchoice)
-}
+	jq('td.selectable').click(rendezvous.toggleuserchoice);
+};
 
 rendezvous.toggleuserchoice = function(){
 	var cell = jq(this);
 	var input = cell.children('input');
 	var span = cell.children('span');
-	if(input.attr('value') == ''){
+	if(input.attr('value') === ''){
 		/* select value */
 		cell.removeClass('unavailable');
 		cell.removeClass('unknown');
@@ -40,24 +39,24 @@ rendezvous.toggleuserchoice = function(){
 		input.attr('value', '');
 		span.html('');
 	}
-}
+};
 
 rendezvous.startdatechoice = function(){
 	jq('body').css('cursor', 'pointer');
 	jq('.rendezvous-datechoice').css('color', 'blue');
 	jq('#rendezvous').click(rendezvous.enddatechoice);
 	jq('.rendezvous-datechoice').click(rendezvous.selectdatechoice);
-}
+};
 
 rendezvous.enddatechoice = function(){
 	jq('body').css('cursor', 'default');
 	jq('#rendezvous').unbind('click');
 	jq('.rendezvous-datechoice').removeAttr('style');
-}
+};
 
 rendezvous.selectdatechoice = function(){
 	var base_url = jq('base').attr('href');
 	var date = jq(this).attr('class').split(' ')[1];
 	window.location.href = base_url + '@@rendezvous_create_event?date=' + date;
-}
+};
 jq(document).ready(rendezvous.init);
